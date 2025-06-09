b26601e379b2badd078ff3e33484a53e841a2c33
alexandrecostapbGIT <costa.alexandre@ifpb.edu.br>
2025-06-09 14:39:27 -0300

    Commit incial

---
+++ app/src/main/java/br/com/example/reitoria2025/ListaDeSugestoesActivity.java
@@ -0,0 +1,24 @@
+package br.com.example.reitoria2025;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ListaDeSugestoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lista_de_sugestoes);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
